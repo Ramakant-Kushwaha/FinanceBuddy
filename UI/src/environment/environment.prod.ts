ca54f79a3aea68120fd5c8286e8fcf91e3d1272a
Ramakant Kushwaha <ramakantkushwaha1995@gmail.com>
2025-01-14 09:08:23 +0530

    some changes in env

--- UI/src/environment/environment.prod.ts
+++ UI/src/environment/environment.prod.ts
@@ -1,5 +1,5 @@
 // src/environments/environment.prod.ts (Production)
 export const environment = {
   production: true,
-  baseUrl: 'https://financebuddy-xih2.onrender.com',
+  baseUrl: 'https://financebuddy-xih2.onrender.com/api',
 };
